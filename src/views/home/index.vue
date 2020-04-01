66cc0608ff68660c8efec1ef6ab62e0c4e14d0e7
xuexiaoboo <xuexiaoboo@163.com>
2020-04-01 15:39:11 +0800

    接口完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,78 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <!-- element-ui中的元素名就是他的classname -->
+  <el-container class="home-container">
+    <!-- 首页头部区域 -->
+    <el-header>
+      <div>
+        <!-- 到时候来个png图片当图标 -->
+        <img src="../../assets/images/logo.png" alt />
+        <span>Wow!!聊天</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 首页主体区域 -->
+    <el-container>
+      <!-- 首页侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 首页内容区域 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
+    return {};
   },
   methods: {
     logout() {
       // 清除登录token，实现退出
-      // window.sessionStorage.clear()
-      this.$router.replace('/login')
+      this.$http.get("/users/logout").then(res => {
+        var status = res.data.status;
+        if (status === 0) {
+          this.$router.replace("/login");
+          this.$message.info("退出成功");
+        }
+        else {
+          this.$message.error('退出失败')
+        }
+      });
     }
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
+.home-container {
+  height: 100%;
+}
 
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+}
+
+.el-header div {
+  display: flex;
+  align-items: center;
+}
+
+.el-header img {
+  width: 60px;
+  height: 50px;
+  margin-right: 10px;
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,21 +3,33 @@
     <div class="login_box">
       <!-- avatar -->
       <div class="avatar_box">
-        <img src="../../assets/logo.png" />
+        <img src="../../assets/images/avatar.png" />
       </div>
       <!-- login_form -->
-      <el-form label-width="0px" class="login_form" :model="loginForm" :rules="loginFormRules" ref="loginFormRef">
+      <el-form
+        label-width="0px"
+        class="login_form"
+        :model="loginForm"
+        :rules="loginFormRules"
+        ref="loginFormRef"
+      >
         <!-- userName -->
         <el-form-item prop="username">
-          <el-input prefix-icon="el-icon-user" v-model="loginForm.username"></el-input>
+          <el-input prefix-icon="el-icon-user" v-model="loginForm.username" placeholder="请输入用户名"></el-input>
         </el-form-item>
         <!-- passWord -->
         <el-form-item prop="password">
-          <el-input prefix-icon="el-icon-lock" v-model="loginForm.password" show-password></el-input>
+          <el-input
+            prefix-icon="el-icon-lock"
+            v-model="loginForm.password"
+            show-password
+            placeholder="请输入密码"
+          ></el-input>
         </el-form-item>
         <el-form-item class="btns">
           <el-button type="primary" @click="login">登录</el-button>
-          <el-button type="info" @click="resetLoginForm">重置</el-button>
+          <el-button type="info" @click="register">注册</el-button>
+          <!-- <el-button type="info" @click="resetLoginForm">重置</el-button> -->
         </el-form-item>
       </el-form>
     </div>
@@ -30,20 +42,20 @@ export default {
     return {
       // 登录表单绑定对象
       loginForm: {
-        username: "admin",
-        password: "123456"
+        username: "",
+        password: ""
       },
       // 登录表单验证规则
       loginFormRules: {
         // 验证用户名是否合法
         username: [
           { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+          { min: 1, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
         ],
         // 验证密码是否合法
         password: [
           { required: true, message: "请输入密码", trigger: "blur" },
-          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+          { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" }
         ]
       }
     };
@@ -51,18 +63,37 @@ export default {
 
   methods: {
     // 重置表单
-    resetLoginForm() {
-      // this是这个template模板对象
-      this.$refs.loginFormRef.resetFields()
-    },
+    // resetLoginForm() {
+    //   // this是这个template模板对象
+    //   this.$refs.loginFormRef.resetFields()
+    // },
+
     // 登录
     login() {
       // 登录预验证，element组件方法
-      this.$refs.loginFormRef.validate((valid) => {
-        // 这里之后写登录的请求，以及登陆后用户token的保存
-        this.$message.success('登录成功')
-        this.$router.replace("/home")
-      })
+      this.$refs.loginFormRef.validate(valid => {
+        // 登录请求
+        this.$http
+          .post("/users/login", {
+            username: this.loginForm.username,
+            password: this.loginForm.password
+          })
+          .then(res => {
+            var status = res.data.status;
+            if (status === 0) {
+              this.$message.success("登录成功");
+              this.$router.replace("/home");
+            }
+            else {
+              this.$message.error('用户名或密码错误')
+            }
+          });
+      });
+    },
+
+    // 注册
+    register() {
+      
     }
   }
 };
